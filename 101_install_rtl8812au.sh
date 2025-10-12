92770b22aee4e2f6050f3f22035ff25fc76d3036
L@myterhell.de <L@myterhell.de>
2025-10-12 17:53:17 +0200

    in preparation

--- 101_install_rtl8812au.sh
+++ 101_install_rtl8812au.sh
@@ -2,5 +2,6 @@
 echo "perform sudo apt-get -y install linux-headers-$(uname -r)"
 sudo apt-get -y install linux-headers-$(uname -r)||echo "failed: sudo apt-get -y install linux-headers-$(uname -r)"
 cd ./WiFi-sniffer/rtl8812au
+sudo rm -r /var/lib/dkms/8812au
 sudo make dkms_install
 
