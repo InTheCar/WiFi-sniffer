c34dcf3cacb41e3d809d3b3d5f061f0766db0349
L@myterhell.de <L@myterhell.de>
2025-10-13 10:20:15 +0200

    in preparation

--- sniffer.sh
+++ sniffer.sh
@@ -46,7 +46,7 @@ echo ""
 echo "Set $1 to monitor mode ..."
 sudo iwconfig $1  mode monitor
 status=$?
-counter=5
+counter=3
 while [ $status -gt 0 ] && [ $counter -gt 0 ]
 do
 echo "Startup $1 ..."
@@ -57,7 +57,7 @@ sudo ifconfig $1 down
 echo "Set $1 to monitor mode ..."
 sudo iwconfig $1 mode monitor
 status=$?
-counter=$counter - 1
+counter=$[counter + 1]
 done
 
 if [ $counter -eq 0 ]; then
